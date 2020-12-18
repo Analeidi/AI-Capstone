3cf28de9dccf64877ca2d67038130da103ab099c
Corey Pieper <26394708+coreyjpieper@users.noreply.github.com>
2020-12-17 22:11:56 -0600

    Add functionality for removing non-english comments

--- chatbot_database.py
+++ chatbot_database.py
@@ -5,14 +5,19 @@ import time
 import os
 from os import path
 
-timeframe = '2009-05'
+timeframe = '2020-04-01'
 sql_transaction = []
 start_row = 0
 cleanup = 1000000
+num_non_english = 0
 
 connection = sqlite3.connect('{}.db'.format(timeframe))
 c = connection.cursor()
 
+# blacklist any subreddits you don't want to include here, these subreddits are popular non-english ones
+skip_subreddits = {"Austria", "argentina", "brasil", "brasilivre", "Bundesliga", "chile", "China_irl", "Colombia", "coronabr", "croatia", "de", "Denmark",
+                   "espanol", "es", "france", "futbol", "greece", "hungary", "italy", "liberta", "mexico", "Pikabu", "Polska", "portugal", "PortugalVegan", "Quebec", "serbia",
+                   "Suomi", "sweden", "uruguay"}
 
 
 def create_table():
@@ -22,20 +27,22 @@ def create_table():
 
 
 def format_data(data):
-    data = data.replace('\n', ' ').replace('\r', ' ').replace('"', "'").replace('&lt;', '<').replace('&gt;', '>').replace('&amp;', '&')
+    data = data.replace('\n', ' ').replace('\r', ' ').replace('"', "'").replace('&lt;', '<').replace('&gt;', '>').replace('&amp;', '&')\
+        .replace('‘', "'").replace('’', "'").replace('“', "'").replace('”', "'")
     return data
 
 
 def transaction_bldr(sql):
     global sql_transaction
     sql_transaction.append(sql)
-    if len(sql_transaction) > 1000:
+    if len(sql_transaction) > 500:
         c.execute('BEGIN TRANSACTION')
         for s in sql_transaction:
             try:
                 c.execute(s)
-            except:
+            except Exception as e:
                 pass
+                # print(e)
         connection.commit()
         sql_transaction = []
 
@@ -70,7 +77,7 @@ def sql_insert_no_parent(commentid, parentid, comment, subreddit, time, score):
 def acceptable(data):
     if len(data.split(' ')) > 100 or len(data) <= 1:
         return False
-    elif len(data) > 500:
+    elif len(data) > 300:
         return False
     elif data == '[deleted]':
         return False
@@ -108,12 +115,72 @@ def find_existing_score(pid):
         return False
 
 
+def find_foreign_subs():
+    c.execute("""CREATE TABLE IF NOT EXISTS parent_reply 
+        (parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, 
+        comment TEXT, subreddit TEXT, unix INT, score INT, unicode INT)""")
+    rows = 0
+
+    with open("reddit data/RC_{}".format(timeframe), buffering=1000) as f:
+
+        for row in f:
+            rows += 1
+            row = json.loads(row)
+            parent_id = row['parent_id'][3:]
+            body = format_data(row['body'])
+            created_utc = row['created_utc']
+            score = row['score']
+            subreddit = row['subreddit']
+            comment_id = row['id']
+
+            unicode = 0  #
+            if body:
+                unicode = max([ord(i) for i in body])
+
+            if unicode >= 191:
+                parent_data = find_parent(parent_id)
+
+                if parent_data:
+                    try:
+                        sql = """INSERT INTO parent_reply (parent_id, comment_id, parent, comment, subreddit, unix, score, unicode) VALUES ("{}","{}","{}","{}","{}",{},{},{});""".format(
+                            parent_id, comment_id, parent_data, body, subreddit, int(created_utc), score, unicode)
+                        transaction_bldr(sql)
+                    except Exception as e:
+                        print('s0 insertion', str(e))
+                else:
+                    try:
+                        sql = """INSERT INTO parent_reply (parent_id, comment_id, comment, subreddit, unix, score, unicode) VALUES ("{}","{}","{}","{}",{},{},{});""".format(
+                            parent_id, comment_id, body, subreddit, int(created_utc), score, unicode)
+                        transaction_bldr(sql)
+                    except Exception as e:
+                        print('s1 insertion', str(e))
+
+            if rows % 100000 == 0:
+                print("Total rows read: {:,}".format(rows))
+
+    print(f"Rows read: {rows}")
+
+
+def non_english(data):
+    global num_non_english
+    if subreddit in skip_subreddits:
+        return True
+    for char in data:
+        if 191 <= ord(char) < 233 or 233 < ord(char):
+            num_non_english += 1
+            if num_non_english % 1000 == 0:
+                print(f"Removed potentially non-english comment: {data}")
+            return True
+    return False
+
+
 if __name__ == "__main__":
     print(os.getcwd())
     create_table()
     row_counter = 0
     paired_rows = 0
     insertions = 0
+    skips = 0
 
     # The data below is being encoded with latin-1 not utf-8
     with open("reddit data/RC_{}".format(timeframe), buffering=1000) as f:
@@ -122,14 +189,18 @@ if __name__ == "__main__":
         for row in f:
             row_counter += 1
             row = json.loads(row)
-            parent_id = row['parent_id']
+            parent_id = row['parent_id'][3:]
             body = format_data(row['body'])
             created_utc = row['created_utc']
             score = row['score']
             subreddit = row['subreddit']
-            comment_id = row['name']
+            comment_id = row['id']
 
             if acceptable(body) and score >= 3:
+                if non_english(body):
+                    skips += 1
+                    continue
+
                 existing_comment_score = find_existing_score(parent_id)
                 parent_data = find_parent(parent_id)
 
@@ -152,7 +223,7 @@ if __name__ == "__main__":
         print(f"SUMMARY FOR {timeframe}")
         print(f"Elapsed time: {end_t - start_t:.2f} seconds")
         pair_percent = round(paired_rows / row_counter * 100, 2)
-        print(f"Total comments read: {row_counter:,}, Comments inserted: {insertions:,}, Comment pairs: {paired_rows:,}")
+        print(f"Total comments read: {row_counter:,}, Comments inserted: {insertions:,}, Comment pairs: {paired_rows:,}, Skips: {skips:,}")
         print(f"Percent inserted: {insertions / row_counter:.2%}, Percent paired: {paired_rows / insertions:.2%}")
         file_path = path.join(os.getcwd(), "reddit data/RC_{}".format(timeframe))
         size = os.stat(file_path).st_size  # size in bytes
